2db74d5d7b9686269d1c35a29f461175a9a74f04
Yaser Alshikh <31512029+yaseralshikh@users.noreply.github.com>
2025-03-23 15:04:33 +0300

    Add files via upload

---
+++ vfs_fonts.js
@@ -0,0 +1,13 @@
+
+pdfMake.vfs = {
+  "Cairo-Regular.ttf": "<BASE64_ENCODED_FONT>"
+};
+
+pdfMake.fonts = {
+  Cairo: {
+    normal: "Cairo-Regular.ttf",
+    bold: "Cairo-Regular.ttf",
+    italics: "Cairo-Regular.ttf",
+    bolditalics: "Cairo-Regular.ttf"
+  }
+};
